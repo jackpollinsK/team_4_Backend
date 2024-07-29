d4d921ef36364fc04619a543ec452a3c3fb0c16a
PhilipGardinerK <philip.gardiner@Kainos.com>
2024-07-29 11:11:48 +0100

    Dao class

--- src/main/java/org/example/daos/ApplicationDao.java
+++ src/main/java/org/example/daos/ApplicationDao.java
@@ -1,11 +1,40 @@
 package org.example.daos;
 
+import org.example.exceptions.DatabaseConnectionException;
 import org.example.models.ApplicationRequest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ApplicationDao {
 
-    public int createDeliveryEmployee(
-            final ApplicationRequest applicationRequest) {
+    public int createApplication(
+            final ApplicationRequest applicationRequest)
+            throws DatabaseConnectionException, SQLException {
+        Connection c = DatabaseConnector.getConnection();
+
+        String insertStatement =
+                "INSERT INTO roleApplication VALUES "
+                        + "(?, ?, ?);";
+
+        PreparedStatement st = c.prepareStatement(insertStatement,
+                Statement.RETURN_GENERATED_KEYS);
+
+        st.setString(1, applicationRequest.getEmail());
+        st.setInt(2, applicationRequest.getRoleID());
+        st.setString(3, applicationRequest.getCvLink());
+
+        st.executeUpdate();
+
+        ResultSet rs = st.getGeneratedKeys();
+
+        if (rs.next()) {
+            return rs.getInt(1);
+        }
+
         return -1;
     }
 }
