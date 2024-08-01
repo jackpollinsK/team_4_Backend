9668e014e4b46c6b8b9295a420a265977596faa2
PhilipGardinerK <philip.gardiner@Kainos.com>
2024-08-01 08:51:40 +0100

    JobRole dao updated

--- src/main/java/org/example/daos/JobRoleDao.java
+++ src/main/java/org/example/daos/JobRoleDao.java
@@ -2,8 +2,10 @@ package org.example.daos;
 
 import org.example.exceptions.DatabaseConnectionException;
 import org.example.models.JobRole;
+import org.example.models.JobRoleRequest;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,38 +17,64 @@ public class JobRoleDao {
             throws SQLException, DatabaseConnectionException {
         List<JobRole> jobRoles = new ArrayList<>();
 
-            Statement statement = c.createStatement();
+        Statement statement = c.createStatement();
 
-            ResultSet resultSet = statement.executeQuery(
-                    "SELECT "
-                            + "jr.id, "
-                            + "jr.role_name AS `RoleName`, "
-                            + "l.name AS `Location`, "
-                            + "c.name AS `Capability`, "
-                            + "b.name AS `Band`, "
-                            + "jr.closing_date AS `ClosingDate` "
-                            + "FROM jobRoles jr "
-                            + "INNER JOIN location l ON jr.location_id = l.id "
-                            + "INNER JOIN capability c "
-                            + "ON jr.capability_id = c.id "
-                            + "INNER JOIN band b ON jr.band_id = b.id "
-                            + "WHERE jr.status = 'open';"
-            );
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT " + "jr.id, " + "jr.role_name AS `RoleName`, "
+                        + "l.name AS `Location`, " + "c.name AS `Capability`, "
+                        + "b.name AS `Band`, "
+                        + "jr.closing_date AS `ClosingDate` "
+                        + "FROM jobRoles jr "
+                        + "INNER JOIN location l ON jr.location_id = l.id "
+                        + "INNER JOIN capability c "
+                        + "ON jr.capability_id = c.id "
+                        + "INNER JOIN band b ON jr.band_id = b.id "
+                        + "WHERE jr.status = 'open';");
 
-            while (resultSet.next()) {
-                JobRole jobRole = new JobRole(
-                        resultSet.getInt("id"),
-                        resultSet.getString("RoleName"),
-                        resultSet.getString("Location"),
-                        resultSet.getString("Capability"),
-                        resultSet.getString("Band"),
-                        resultSet.getDate("ClosingDate"),
-                        "open"
-                );
+        while (resultSet.next()) {
+            JobRole jobRole = new JobRole(resultSet.getInt("id"),
+                    resultSet.getString("RoleName"),
+                    resultSet.getString("Location"),
+                    resultSet.getString("Capability"),
+                    resultSet.getString("Band"),
+                    resultSet.getDate("ClosingDate"), "open");
 
-                jobRoles.add(jobRole);
+            jobRoles.add(jobRole);
         }
 
         return jobRoles;
     }
+
+    public int insertRole(final JobRoleRequest jobRoleRequest,
+                          final Connection c) throws SQLException {
+        String insertRoleQuery =
+                "INSERT INTO jobRoles(role_name, location_id, capability_id, "
+                        + "band_id, closing_date, status) VALUES "
+                        + "('Test', 1, 1, 1, '2000-01-01', 'open');";
+
+        PreparedStatement preparedStmt = c.prepareStatement(insertRoleQuery,
+                Statement.RETURN_GENERATED_KEYS);
+        preparedStmt.setString(1, jobRoleRequest.getRoleName());
+        preparedStmt.setString(2, jobRoleRequest.getLocation());
+        preparedStmt.setString(3, jobRoleRequest.getCapability());
+        preparedStmt.setString(4, jobRoleRequest.getBand());
+        preparedStmt.setString(5, jobRoleRequest.getClosingDate().toString());
+
+        int affectedRows = preparedStmt.executeUpdate();
+
+        if (affectedRows == 0) {
+            throw new SQLException("Creating user failed, no rows affected.");
+        }
+
+        int roleID = 0;
+
+        try (ResultSet rs = preparedStmt.getGeneratedKeys()) {
+            if (rs.next()) {
+                roleID = rs.getInt(1);
+            }
+        }
+
+        return roleID;
+    }
+
 }
