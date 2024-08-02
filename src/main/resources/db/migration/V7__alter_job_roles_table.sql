65e675bdfcb2dd3c3d79fbd8d9887a8f0422e5c5
jackpollinsK <jackpollins@kainos.com>
2024-08-02 09:52:18 +0100

    fixing bugs

--- src/main/resources/db/migration/V7__alter_job_roles_table.sql
+++ src/main/resources/db/migration/V7__alter_job_roles_table.sql
@@ -4,20 +4,20 @@ ADD COLUMN responsibilities VARCHAR(1000) NOT NULL,
 ADD COLUMN job_spec VARCHAR(500) NOT NULL;
 
 UPDATE jobRoles SET
-description = 'Sample Description',
-responsibilities = 'Sample Responsibilities',
+description = 'The role of a Software Engineer – Engineering Apprentice is a dynamic entry-level position designed for those eager to launch their career in software development within a forward-thinking technology company. Based in Belfast, this apprenticeship provides an exceptional opportunity to gain hands-on experience in a professional software engineering environment. The position is ideal for recent graduates or individuals transitioning into the tech industry, offering a blend of practical work experience and formal training.',
+responsibilities = 'As a Software Engineer – Engineering Apprentice, your responsibilities will be multifaceted, designed to provide a thorough introduction to software development while contributing to the success of ongoing projects. Initially, your primary focus will be on learning and development. You will engage in structured training programs to build a foundational understanding of software engineering principles, programming languages, and development tools.',
 job_spec = 'https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Apprentice%20Software%20Engineer%20%28Apprentice%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1'
 WHERE id = 1;
 
 UPDATE jobRoles SET
-description = 'Sample Description',
-responsibilities = 'Sample Responsibilities',
+description = 'The role of a Software Engineer – Engineering Apprentice is a dynamic entry-level position designed for those eager to launch their career in software development within a forward-thinking technology company. Based in Belfast, this apprenticeship provides an exceptional opportunity to gain hands-on experience in a professional software engineering environment. The position is ideal for recent graduates or individuals transitioning into the tech industry, offering a blend of practical work experience and formal training.',
+responsibilities = 'As a Software Engineer – Engineering Apprentice, your responsibilities will be multifaceted, designed to provide a thorough introduction to software development while contributing to the success of ongoing projects. Initially, your primary focus will be on learning and development. You will engage in structured training programs to build a foundational understanding of software engineering principles, programming languages, and development tools.',
 job_spec = 'https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Software%20Engineer%20%28Trainee%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1'
 WHERE id = 2;
 
 UPDATE jobRoles SET
-description = 'Sample Description',
-responsibilities = 'Sample Responsibilities',
+description = 'The role of a Software Engineer – Engineering Apprentice is a dynamic entry-level position designed for those eager to launch their career in software development within a forward-thinking technology company. Based in Belfast, this apprenticeship provides an exceptional opportunity to gain hands-on experience in a professional software engineering environment. The position is ideal for recent graduates or individuals transitioning into the tech industry, offering a blend of practical work experience and formal training.',
+responsibilities = 'As a Software Engineer – Engineering Apprentice, your responsibilities will be multifaceted, designed to provide a thorough introduction to software development while contributing to the success of ongoing projects. Initially, your primary focus will be on learning and development. You will engage in structured training programs to build a foundational understanding of software engineering principles, programming languages, and development tools.',
 job_spec = 'https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Software%20Engineer%20%28Associate%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1'
 WHERE id = 3;
 
