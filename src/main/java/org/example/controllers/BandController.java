d44c89dc86645b6fcff3d132fa3634a9ead4324d
PhilipGardinerK <philip.gardiner@Kainos.com>
2024-08-05 15:02:05 +0100

    Controllers

---
+++ src/main/java/org/example/controllers/BandController.java
@@ -0,0 +1,46 @@
+package org.example.controllers;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import org.example.daos.BandDao;
+import org.example.exceptions.DatabaseConnectionException;
+import org.example.models.JobRole;
+import org.example.models.UserRole;
+import org.example.services.BandService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+
+@Api("Band API")
+@Path("/api/JobRoles")
+public class BandController {
+
+    BandService bandService;
+
+    public BandController(final BandService bandService) {
+        this.bandService = bandService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN})
+    @ApiOperation(
+            value = "Returns Bands",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = JobRole.class)
+    public Response getBands() {
+        try {
+            return Response.ok().entity(bandService.getBands()).build();
+        } catch (SQLException | DatabaseConnectionException e) {
+            return Response.serverError().build();
+        }
+    }
+}
---
+++ src/main/java/org/example/controllers/LocationController.java
@@ -0,0 +1,45 @@
+package org.example.controllers;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import org.example.exceptions.DatabaseConnectionException;
+import org.example.models.JobRole;
+import org.example.models.UserRole;
+import org.example.services.LocationService;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+
+@Api("Location API")
+@Path("/api/location")
+public class LocationController {
+
+    final LocationService locationService;
+
+    public LocationController(final LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN})
+    @ApiOperation(
+            value = "Returns Locations",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = JobRole.class)
+    public Response getLocations() {
+        try {
+            return Response.ok().entity(locationService.getLocations()).build();
+        } catch (SQLException | DatabaseConnectionException e) {
+            return Response.serverError().build();
+        }
+    }
+}
