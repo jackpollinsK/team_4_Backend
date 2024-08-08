8c19b300191b08c14baf4e105dd794057522903c
jackpollinsK <jackpollins@kainos.com>
2024-08-08 15:41:34 +0100

    fixes

--- src/main/resources/db/migration/V11__adding_sample_data_for_ai_chat.sql
+++ src/main/resources/db/migration/V11__adding_sample_data_for_ai_chat.sql
@@ -1,10 +1,11 @@
-INSERT INTO jobRoles (role_Name, location_id, capability_id, band_id, closing_Date, description, responsibilities, job_Spec, open_Positions)
+INSERT INTO jobRoles (role_Name, location_id, capability_id, band_id, closing_Date, status, description, responsibilities, job_Spec, open_Positions)
 SELECT
     CONCAT(c.name, ' ', b.name, ' Role') AS roleName,
     l.id AS location_id,
     c.id AS capability_id,
     b.id AS band_id,
     '2024-12-31' AS closingDate,
+    'open' as status,
     CONCAT('Job description for ', c.name, ' in ', l.name, ' at ', b.name, ' level.') AS description,
     CONCAT('Responsibilities for ', c.name, ' role.') AS responsibilities,
     'https://kainossoftwareltd.sharepoint.com/people/SitePages/Career-Lattice.aspx' AS jobSpec,
