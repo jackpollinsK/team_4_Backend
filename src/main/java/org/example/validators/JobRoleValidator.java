e91dec5a8df8c5350f1cca612f91d537d1c160c1
jackpollinsK <jackpollins@kainos.com>
2024-08-08 14:27:42 +0100

    validator test

--- src/main/java/org/example/validators/JobRoleValidator.java
+++ src/main/java/org/example/validators/JobRoleValidator.java
@@ -9,33 +9,39 @@ public class JobRoleValidator {
 
     JobRoleDao jobRoleDao;
 
-    public void validateJobRole(final JobRoleRequest jobRoleRequest) throws
-            InvalidException {
+    public void validateJobRole(final JobRoleRequest jobRoleRequest)
+            throws InvalidException {
+        if (jobRoleRequest.getRoleName() == null) {
+            throw new InvalidException(Entity.JOB_ROLE,
+                    "Role name must be not be null");
+        }
         if (jobRoleRequest.getRoleName().length() > 100) {
             throw new InvalidException(Entity.JOB_ROLE,
                     "Role name must be less than 100 Characters");
         }
-        if (jobRoleRequest.getRoleName() == null) {
+
+        if (jobRoleRequest.getDescription() == null) {
             throw new InvalidException(Entity.JOB_ROLE,
-                    "Role name must be not be null");
+                    "Description name must be not be null");
         }
         if (jobRoleRequest.getDescription().length() > 2000) {
             throw new InvalidException(Entity.JOB_ROLE,
                     "Description name must be less than 2000 Characters");
         }
-        if (jobRoleRequest.getDescription() == null) {
+
+        if (jobRoleRequest.getResponsibilities() == null) {
             throw new InvalidException(Entity.JOB_ROLE,
-                    "Description name must be not be null");
+                    "Responsibility name must be not be null");
         }
         if (jobRoleRequest.getResponsibilities().length() > 1000) {
             throw new InvalidException(Entity.JOB_ROLE,
                     "Responsibility name must be less than 1000 Characters");
         }
-        if (jobRoleRequest.getResponsibilities() == null) {
-            throw new InvalidException(Entity.JOB_ROLE,
-                    "Responsibility name must be not be null");
-        }
 
+        if (jobRoleRequest.getJobSpec() == null) {
+            throw new InvalidException(Entity.JOB_ROLE,
+                    "Job Spec name must be not be null");
+        }
         if (jobRoleRequest.getJobSpec().length() > 500) {
             throw new InvalidException(Entity.JOB_ROLE,
                     "Job Spec name must be less than 500 Characters");
